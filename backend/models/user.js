9ab43c3022d864bbad0fd800d6283f53e467fa3a
Alex D <alex.darincev@gmail.com>
2022-06-28 00:19:38 +0300

    (auth): enabled password hash  entry return  within user data by API during authentication

--- backend/models/user.js
+++ backend/models/user.js
@@ -52,17 +52,19 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new LoginError('Incorrect password or email'));
-    }
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new LoginError('Incorrect password or email'));
       }
-      return user;
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new LoginError('Incorrect password or email'));
+        }
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model('user', userSchema);
