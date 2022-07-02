fcd48a590f00c38f86e024c823544b60703f0be8
Alex D <alex.darincev@gmail.com>
2022-07-03 00:10:45 +0300

    enabled pre-route validation for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { validateURL } = require('../validators/validators');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -11,7 +13,24 @@ const {
 router.get('/', getUsers);
 //also applied for GET /users/me
 router.get('/:userId', getUserById);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required.min(2).max(30),
+    }),
+  }),
+  updateUser
+);
+router.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().required().custom(validateURL),
+    }),
+  }),
+  updateAvatar
+);
 
 module.exports = router;
