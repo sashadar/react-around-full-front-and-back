1c9bc1eb16fd31abde8eb4b92369970c0a51bfaf
Alex D <alex.darincev@gmail.com>
2022-07-03 02:12:49 +0300

    added request and error loggers

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,12 @@ const cors = require('cors');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
-const auth = require('./middleware/auth');
 
+const auth = require('./middleware/auth');
 const errorHandler = require('./middleware/errorHandler');
+const { requestLogger, errorLogger } = require('./middleware/logger');
+
+const NotFoundError = require('./errors/notfounderror');
 
 const { PORT = 3000 } = process.env;
 
@@ -33,17 +36,20 @@ mongoose.connect('mongodb://localhost:27017/aroundb7', {
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
 
 app.use('/cards', auth, cardsRouter);
 app.use('/users', auth, usersRouter);
+
 app.use('/', (req, res) => {
-  res.status(404);
-  res.send(JSON.stringify({ message: 'Requested resource not found' }));
+  throw new NotFoundError('Requested resource not found');
 });
 
+app.use(errorLogger);
+app.use(errors());
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
