f8f7611b4a88f7919ad4eb4d0a4fd68857214d6e
Alex D <alex.darincev@gmail.com>
2022-07-03 01:51:22 +0300

    enabled pre-route validation for cards

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { validateURL } = require('../validators/validators');
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   addCard,
@@ -8,9 +10,43 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', addCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required.custom(validateURL),
+    }),
+  }),
+  addCard
+);
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().hex(),
+    }),
+  }),
+  deleteCard
+);
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().hex(),
+    }),
+  }),
+  likeCard
+);
+
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().hex(),
+    }),
+  }),
+  dislikeCard
+);
 
 module.exports = router;
