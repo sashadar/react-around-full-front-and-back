6585920c1d60e12f6fe1dbbc5b7058d552ad114a
Alex D <alex.darincev@gmail.com>
2022-07-07 15:13:21 +0300

    enabled request number limitations using express-rate-limit package

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -21,6 +22,13 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 mongoose.connect('mongodb://localhost:27017/aroundb', {
   useNewUrlParser: true,
 });
@@ -30,6 +38,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.options('*', cors());
 app.use(requestLogger);
+app.use(limiter);
 
 //should be removed after passing the review
 app.get('/crash-test', () => {
